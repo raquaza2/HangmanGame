e6624c50cddfef4e11e006b49154603128073ed4
raquaza2 <57903323+raquaza2@users.noreply.github.com>
2024-10-14 23:15:06 +0800

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -33,20 +33,39 @@ hangman_art = {0:("   ",
 def display_man(wrong_guesses):
     for line in hangman_art[wrong_guesses]:
      print(line)
+
 def display_hint(hint):
     print(" ".join(hint))
+     
+
 def display_answer(answer):
     print(" ".join(answer))
+  
+
+def main():
+    
     answer = random.choice(words)
     hint = ["_"] * len(answer)
+    wrong_guesses = 0
+    guessed_letters = set()
+    is_running = True
+    
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+         
+        guess = input("Enter a letter: ").lower()
+        
         if len(guess) !=1 or not guess.isalpha:
             print("Please enter a single letter.")
             continue
+        
         if guess in guessed_letters:
             print(f"{guess} is already guessed")
             continue
             
         guessed_letters.add(guess)
+        
         if guess in answer:
              for i in range(len(answer)):
               if answer[i] == guess:
